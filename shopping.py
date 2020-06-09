993e8982a75c565ff13e86b138533b7876cbbc8c
KML122089 <65563104+KML122089@users.noreply.github.com>
2020-06-09 17:54:11 -0400

    Conditional Product Loop

--- shopping.py
+++ shopping.py
@@ -45,16 +45,18 @@ def to_usd(my_price):
 # print ("Checkout at:", now.strftime("%Y-%m-%d %H:%M %p"))
 
 print("---------------------------------")
-#print("Please input a product identifier:")
-x = input("Please input a product identifier:") #> this is a string
-# print(x)
-# print(type(x))
-matching_products = [p for p in products if str(p["id"]) == str(x)]
-matching_product = matching_products[0]
-print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+
+while True:
+    selected_id = input("Please input a product identifier:") #> this is a string
+    if selected_id == "DONE":
+        break
+    else:
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        matching_product = matching_products[0]
+        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 
-# if x == "done":
+
 #     print("---------------------------------")
 #     print("Kelly's Organics")
 #     print("www.KellysOrganics.com")
