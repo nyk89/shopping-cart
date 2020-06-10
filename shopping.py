04a0b4a6be1200cffb6e0cd7a78ff18169b6ef82
KML122089 <65563104+KML122089@users.noreply.github.com>
2020-06-10 11:45:10 -0400

    Sum total price

--- shopping.py
+++ shopping.py
@@ -45,6 +45,7 @@ def to_usd(my_price):
 # print ("Checkout at:", now.strftime("%Y-%m-%d %H:%M %p"))
 
 print("---------------------------------")
+total_price = 0
 
 while True:
     selected_id = input("Please input a product identifier:") #> this is a string
@@ -53,8 +54,11 @@ while True:
     else:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
         print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
+
+print("TOTAL PRICE: " + str(total_price)) #> format as USD
 
 
 #     print("---------------------------------")
